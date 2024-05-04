a05f36c23b507aaed84d3d0fcb027b213a457fa6
Trevor Duhon <taduhon1485@gmail.com>
2024-05-03 20:55:11 -0500

    Completed Step One 'EightBall Component' as well as Further Study 1 'Reset'

--- src/EightBall.jsx
+++ src/EightBall.jsx
@@ -1,5 +1,32 @@
+import { useState } from 'react'
+import './EightBall.css'
 
-const EightBall = () => {}
+const EightBall = ({answers}) => {
+  const getRandIdx = () => Math.floor(Math.random() * 20);
+
+  const reset = () => {
+    setBallColor('black');
+    setBallText('Think of a Question');
+  }
+
+  const getAnswers = () => {
+    let idx = getRandIdx();
+    console.log(idx);
+    setBallColor(answers[idx].color);
+    setBallText(answers[idx].msg);
+  }
+  const [ballColor, setBallColor] = useState('black');
+  const [ballText, setBallText] = useState('Think of a Question');
+
+  return (
+    <div className='EightBall'>
+      <button onClick={getAnswers} className='EightBall-button' style={{backgroundColor: ballColor}}>
+        {ballText}
+      </button>
+      <button className='EightBall-reset' onClick={reset}>Reset</button>
+    </div>
+  )
+}
 
 
 
