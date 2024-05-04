ec4b8f5402a303715dd9c7e04b949d10b503e4fc
Trevor Duhon <taduhon1485@gmail.com>
2024-05-03 20:03:56 -0500

    Initialized git repo for React State 'Eight Ball' Exercise

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import EightBall from './EightBall'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <EightBall />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/EightBall.jsx
@@ -0,0 +1,6 @@
+
+const EightBall = () => {}
+
+
+
+export default EightBall;
